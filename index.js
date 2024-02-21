0a5327588afe0b19cb1ef629cd1c93d1dcec6950
Arey-7 <aaronmulandi@gmail.com>
2024-02-21 20:55:01 +0300

    Here it is

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import cors from "cors";
+import axios from "axios";
+import dotenv  from "dotenv"
+
+const PORT = 777;
+dotenv.config(); // require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+
+const URL = process.env.API_URL;
+const API_KEY = process.env.API_KEY;
+
+
+app.get('/food', async (req, res) => {
+    const { query } = req.query
+    try {
+        const response = await axios.get(URL, {
+            params: {
+                query,
+                apiKey: API_KEY
+            }
+        });
+        const data = response.data.results;
+        res.json(data);
+    } catch (error) {
+        console.error("Error fetching data:", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
+})
+
+app.get('/', (req, res) => {
+    res.json('hi')
+})
+
+app.listen(777, () => console.log(`Server is running on port ${PORT}`));
